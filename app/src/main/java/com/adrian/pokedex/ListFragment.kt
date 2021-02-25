4ce0eef030b235fb08c35fe26ce9194a0ca4e659
Adrian Millan <adrian231998@gmail.com>
2021-02-25 11:25:27 -0600

    uno de los Fragments es una lista de todos los pokemons disponibles, le implementamos su Recycler View junto con su Adapter, creamos la clase Pokemon, instanciamos algunos y los insertamos en el Recycler View

--- app/src/main/java/com/adrian/pokedex/ListFragment.kt
+++ app/src/main/java/com/adrian/pokedex/ListFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListFragment : Fragment() {
 
@@ -13,7 +16,31 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view= inflater.inflate(R.layout.fragment_list, container, false)
+        val recycler= view.findViewById<RecyclerView>(R.id.pokemon_recycler)
+        recycler.layoutManager= LinearLayoutManager(requireActivity())
+        val adapter= PokemonAdapter()
+        recycler.adapter=adapter
+
+        adapter.onItemClickLitener={
+            Toast.makeText(requireActivity(), it.name, Toast.LENGTH_SHORT).show()
+        }
+
+        val pokemonList= mutableListOf(
+                Pokemon(1,"Bulbasaur",55,49, 19,45, Pokemon.PokemonType.GRASS),
+                Pokemon(2,"Ivysaur",46,91, 23,23, Pokemon.PokemonType.GRASS),
+                Pokemon(3,"Venuasaur",31,10, 45,25, Pokemon.PokemonType.GRASS),
+                Pokemon(4,"Charmander",15,18, 29,45, Pokemon.PokemonType.FIRE),
+                Pokemon(5,"Charmeleon",25,65, 49,65, Pokemon.PokemonType.FIRE),
+                Pokemon(6,"Charizzard",45,34, 90,78, Pokemon.PokemonType.FIRE),
+                Pokemon(7,"Squirtle",56,12, 32,56, Pokemon.PokemonType.WATER),
+                Pokemon(8,"warturtle",67,56, 67,26, Pokemon.PokemonType.WATER),
+                Pokemon(9,"Blastoise",98,39, 78,74, Pokemon.PokemonType.WATER),
+                Pokemon(10,"Pikachu",16,34, 32,12, Pokemon.PokemonType.ELECTRIC),
+        )
+        adapter.submitList(pokemonList)
+
+        return view
     }
 
 
